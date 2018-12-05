d55ad9f2cc4111eac72afac37139472d2623b86f
hosamalden5 <45264357+hosamalden5@users.noreply.github.com>
2018-12-05 09:02:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -115,9 +115,34 @@ spammer4.on('ready', () => {
   spammer4.guilds.get(server).channels.get(channel).send("#daily")
   },700000);
 });
-
+spammer5.on('ready', () => {
+    console.log(`Logged in as ${spammer5.user.tag}!`);
+  var server = "509609934472871936";
+  var channel = "509609934472871938";
+  var spamwords = ["spapspadpsapdmsdpmsofjdisahfudoshfaudsigfiudasgfiydDFSFASDF", "FDASFAOSDJFIODSAFCUYN78NFY87FDSIOFDSUA89FCY78EWFIOUWE9F", "a7a al 4ebe4 da s3d3sa5d6s74dfsafads", 
+  "كسمك يا جلال كسشمينملبيل كسم جلال احا يسطى", "GFDGFDGSDFGFDS GFDSG FDGK,[SDFP,G بيسحيجحلنبجيسحل506546 8954698-0لخني", "كطبينخحلتبيهسختلع8هعةلبيس89ةغل7ف", 
+  "874fads89f74d98sf74d8s94fg798d4g98fd4 g98f4dsg6fd4sg98 4sdfg84 fsd", "a7a al 4eb4 da3 a7a da kan be soba3 a7a da kan gded", "ksomk al cc da ebn klb 3rs ws5 4rmoot", 
+  `‏نقدم لك دعوة لسموك الراقيَ . \n
+  ‏لنحظى بجمال حضورك وتواجدك\n
+  •بتنـورونـا الجميع :wink:\n
+  •اي مشكله توآجهك ولا يهمك. عندك الاداره تفضل ونحل مشكلتك مع آي شخص بالسيرفر.:telephone_receiver:\n
+  •عندك فكره موضوع تفضل شآركنا فيه:jack_o_lantern::gift:\n
+  •واخيرا حييييياكم واكيد وجودكم هو اللي بيدعمنا وراح نكون الافضل.\n
+  شرفنا يا عسل:wink: :kissing_heart:\n`, `fuck , you pr \n credit miner here \n will fuck pro bot`];
+  let timer = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000
+  setInterval(() => {
+  let spam = `${spamwords[Math.floor(Math.random() * spamwords.length)]}`;
+  spammer5.guilds.get(server).channels.get(channel).send(`${spam}`)
+  },`${timer}`);
+  setInterval(() => {
+  spammer5.guilds.get(server).channels.get(channel).send("#daily")
+  },700000);
+  
+});
 ////////////
 spammer1.login(process.env.SPAM1_TOKEN)
 spammer2.login(process.env.SPAM2_TOKEN)
 spammer3.login(process.env.SPAM3_TOKEN)
 spammer4.login(process.env.SPAM4_TOKEN)
+spammer5.login(process.env.SPAM5_TOKEN)
+
